6dc4e81c5c6188c5230ce2ccc0a9bb685c71c003
Juan Alberto Molinas Ihara <jotamolinas@gmail.com>
2026-08-15 14:12:09 +0900

    Delete patch_rules.cjs

--- patch_rules.cjs
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-let code = fs.readFileSync('firestore.rules', 'utf-8');
-
-code = code.replace(
-  "request.auth.token.email != null",
-  "\"email\" in request.auth.token"
-);
-
-fs.writeFileSync('firestore.rules', code);
