bb7646a761fbc96af366347d5ac49bd1d8a93063
Juan Alberto Molinas Ihara <jotamolinas@gmail.com>
2026-08-15 14:11:13 +0900

    Delete patch2.cjs

--- patch2.cjs
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-let code = fs.readFileSync('components/PagaresTab.tsx', 'utf-8');
-
-code = code.replace("const { doc, updateDoc } = require('firebase/firestore');", "");
-code = code.replace("// We must use the imported doc and updateDoc from the file scope.", "");
-code = code.replace("// Wait, we don't need require here, we can just use doc and updateDoc as they are already imported.", "");
-
-fs.writeFileSync('components/PagaresTab.tsx', code);
